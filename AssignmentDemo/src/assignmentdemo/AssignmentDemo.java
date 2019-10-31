3a88ce47cebf333950cbba5a203ceed02d13d48e
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-10-31 17:41:57 +0600

    Add files via upload

---
+++ AssignmentDemo/src/assignmentdemo/AssignmentDemo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignmentdemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class AssignmentDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int x=150 ;
+        int y=15;
+        
+        x+=y;
+        System.out.println("1. x = "+x);
+        
+        x-=y;
+        System.out.println("2. x = "+x);
+        
+        x*=y;
+        System.out.println("3. x = "+x);
+        
+        x/=y;
+        System.out.println("4. x = "+x);
+        
+        x%=y;
+        System.out.println("5. x = "+x);
+        
+    }
+    
+}
