fdfe7a582e92720123c980ee187673cd042601c6
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-11-01 00:09:38 +0600

    Add files via upload

---
+++ DataType/src/datatype/DataType.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatype;
+
+/**
+ *
+ * @author Asus
+ */
+public class DataType {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean b=true;
+        char c='m';
+        int i=10;
+        short s=2345;
+        double d=11.11;
+        float f=10.2f;
+               
+        System.out.printf(" boolean = %b \n",b);
+        System.out.printf(" character = %c \n",c);
+        System.out.printf(" short = %s \n",s);
+        System.out.printf(" integer = %d",i);
+        System.out.printf(" float = %.2f \n",f);
+        System.out.printf(" double  = %.3f \n ",d);
+        
+        
+    }
+    
+}
