9db3f941c086f6a73d611df5480db0d18be8d9a4
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-11-06 23:13:04 +0600

    Add files via upload

---
+++ PatternTriangle4Demo/src/patterntriangle4demo/PatternTriangle4Demo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patterntriangle4demo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class PatternTriangle4Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.print("How Many Line's : ");
+        int n = input.nextInt();
+        
+        for (int row = 5; row >=1; row--) {
+            for (int col = 1; col <=row; col++) {
+               System.out.print(" "+row); 
+            }
+            System.out.println();
+            
+        }
+        
+    }
+    
+}
