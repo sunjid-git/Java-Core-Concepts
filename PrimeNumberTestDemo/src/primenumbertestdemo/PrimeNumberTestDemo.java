06eee8fdffb6f5ad32dc4c5af4d57fc7491433d7
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-11-07 10:24:45 +0600

    Add files via upload

---
+++ PrimeNumberTestDemo/src/primenumbertestdemo/PrimeNumberTestDemo.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primenumbertestdemo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class PrimeNumberTestDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int m,n,count=0;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter Initial Value : ");
+        m = input.nextInt();
+        
+         System.out.print("Enter Ending Value : ");
+        n = input.nextInt();
+        
+        System.out.println("Prime Number : ");
+        
+        for(int i = m; i<=n; i++) {
+            
+            
+            for (int j = 2; j <= i; j++)
+                {  
+                    if(i%j==0)
+                    count++;
+                    break;
+                }
+            
+            if(count==0)
+                
+                System.out.print(" "+i);
+                 count=0;
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
