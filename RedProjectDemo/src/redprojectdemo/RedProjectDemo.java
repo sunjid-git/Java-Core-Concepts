f1150f9b38974b9c0c90f6531284d386d80deaa1
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-11-07 10:26:08 +0600

    Add files via upload

---
+++ RedProjectDemo/src/redprojectdemo/RedProjectDemo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redprojectdemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class RedProjectDemo {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println(" Name  : Md Sunjid Hasan ");
+        System.out.println(" Phone : +880 1521 417063 ");
+        System.out.println(" Email : sunjid@baiust.edu.bd ");
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+}
