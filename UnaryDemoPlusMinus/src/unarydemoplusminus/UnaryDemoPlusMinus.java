dd45ddc059c9a8a7f12ed8c1b11468d990222013
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-11-08 11:15:45 +0600

    Add files via upload

---
+++ UnaryDemoPlusMinus/src/unarydemoplusminus/UnaryDemoPlusMinus.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unarydemoplusminus;
+
+
+
+/**
+ *
+ * @author Asus
+ */
+public class UnaryDemoPlusMinus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x=10,y=-10, result1,result2;
+        
+        result1=+x;
+        result2=+y;
+        
+        System.out.println("Result = "+result1);
+        System.out.println("Result = "+result2);
+        
+         result1=-x;
+         result2=-y;
+        System.out.println("Result = "+result1);
+        System.out.println("Result = "+result2);
+    }
+    
+}
