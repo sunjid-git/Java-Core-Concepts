d3419251f4cc05f38392bb30e8ab67ad0a0c2631
Sunjid Hasan <sunjid.baiust@gmail.com>
2019-11-10 01:39:53 +0600

    Add files via upload

---
+++ Series5Demo/src/series5demo/Series5Demo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package series5demo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class Series5Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n, sum=1; 
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your Series Size : ");
+        
+        n = input.nextInt();
+        
+        for (int i = 1; i < n; i++) {
+            
+            
+            sum = sum *i;
+            
+            System.out.print(i+" ");
+          
+        }
+        System.out.println();
+        System.out.println("Result : "+sum);
+    }
+    
+}
