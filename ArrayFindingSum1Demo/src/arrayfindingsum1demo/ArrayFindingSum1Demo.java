f10e2f1bdc5c130a8759f962fb8f3417dd75908e
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-16 10:19:38 +0600

    Add files via upload

---
+++ ArrayFindingSum1Demo/src/arrayfindingsum1demo/ArrayFindingSum1Demo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayfindingsum1demo;
+
+/**
+ *
+ * @author Asus
+ */
+public class ArrayFindingSum1Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] number = new int[5];
+         
+         number[0]=10;
+         number[1]=20;
+         number[2]=30;
+         number[3]=40;
+         number[4]=50;
+        
+         int sum= number[0]+number[1]+number[2]+number[3]+number[4];
+            System.out.println("Result : "+sum);
+    }
+    
+}
