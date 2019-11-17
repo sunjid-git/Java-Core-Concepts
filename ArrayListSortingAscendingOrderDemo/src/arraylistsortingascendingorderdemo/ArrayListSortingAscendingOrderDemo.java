691f74904dcf36cbc7f034fe5beca29e09c39a37
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-17 10:09:18 +0600

    Add files via upload

---
+++ ArrayListSortingAscendingOrderDemo/src/arraylistsortingascendingorderdemo/ArrayListSortingAscendingOrderDemo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylistsortingascendingorderdemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author Asus
+ */
+public class ArrayListSortingAscendingOrderDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Integer> number = new ArrayList<>();
+        
+        number.add(20);
+        number.add(-3);
+        number.add(18);
+        number.add(92);
+        number.add(0);
+        number.add(2);
+        System.out.println("Array List Before Sorting : "+number);
+        
+        //for ascending
+        Collections.sort(number);
+        
+        System.out.println("Array List After Sorting : "+number);
+        
+        
+    }
+    
+}
