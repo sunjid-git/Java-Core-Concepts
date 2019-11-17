7a1405c8e3f49f9fe76e276bb9491beccc04a6d4
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-17 23:25:01 +0600

    Add files via upload

---
+++ ArrayMatrixUpperTriangleSumDemo/src/arraymatrixuppertrianglesumdemo/ArrayMatrixUpperTriangleSumDemo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraymatrixuppertrianglesumdemo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class ArrayMatrixUpperTriangleSumDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] A= new int[3][3];
+        int SumOfUpperTrangle =0;
+        Scanner input= new Scanner(System.in);
+        
+        //input
+        System.out.println("Input : ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                A[i][j]=input.nextInt();
+            }
+        }
+        //to show
+        System.out.println("Matrix : ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("\t "+A[i][j]);
+            }
+            System.out.print("\n");
+        }
+        //to add diagonal 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+              if(i<j)  
+              {
+                SumOfUpperTrangle=SumOfUpperTrangle+A[i][j];
+              }
+              }
+        }
+        System.out.print("Result : "+SumOfUpperTrangle);
+        System.out.println("\n");
+    }
+    
+}
