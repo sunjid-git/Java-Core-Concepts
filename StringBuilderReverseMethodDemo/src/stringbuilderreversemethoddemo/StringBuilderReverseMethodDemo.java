47a2cbe42020582e4f77c8f2ca3043378cb783bd
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-20 03:46:35 +0600

    Add files via upload

---
+++ StringBuilderReverseMethodDemo/src/stringbuilderreversemethoddemo/StringBuilderReverseMethodDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringbuilderreversemethoddemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringBuilderReverseMethodDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         StringBuilder str = new StringBuilder("Sunjid");
+        System.out.println(str);
+        
+        str.reverse();
+        System.out.println(str);
+    }
+    
+}
