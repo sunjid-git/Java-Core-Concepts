7718c42ee24b1072673c4724cef474b3974039cc
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-20 03:44:47 +0600

    Add files via upload

---
+++ StringBufferStringPassDemo/src/stringbufferstringpassdemo/StringBufferStringPassDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringbufferstringpassdemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringBufferStringPassDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s1 = "Sunjid";
+        StringBuffer s2 = new StringBuffer(s1);
+        System.out.println(s2);
+    }
+    
+}
