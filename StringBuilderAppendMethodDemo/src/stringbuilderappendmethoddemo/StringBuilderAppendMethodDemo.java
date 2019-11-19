a1ab9a3423248e51725e160e3e708eb6e139df0d
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-20 03:45:16 +0600

    Add files via upload

---
+++ StringBuilderAppendMethodDemo/src/stringbuilderappendmethoddemo/StringBuilderAppendMethodDemo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringbuilderappendmethoddemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringBuilderAppendMethodDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StringBuilder str = new StringBuilder("Sunjid");
+        System.out.println(str);
+        
+        str.append(" Hasan");
+        System.out.println(str);
+        
+        str.append(" BAIUSTIAN");
+        System.out.println(str);
+        
+        str.append(" Spring 2016");
+        System.out.println(str);
+        
+    }
+    
+}
