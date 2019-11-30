121c11b70e88d2bd5f09da67f21b8b9fc986e338
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-01 00:42:12 +0600

    Add files via upload

---
+++ StringOutputUsingForLoopDemo/src/stringoutputusingforloopdemo/StringOutputUsingForLoopDemo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringoutputusingforloopdemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringOutputUsingForLoopDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] name = {"Sunjid", "Hasan","Mafi"};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(""+name[i]);
+        }
+    }
+    
+}
