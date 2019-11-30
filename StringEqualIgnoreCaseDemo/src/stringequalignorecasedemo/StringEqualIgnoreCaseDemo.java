ff01f3fb0912368e548fe941fbd6d7f9ca1795aa
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-30 15:21:18 +0600

    Add files via upload

---
+++ StringEqualIgnoreCaseDemo/src/stringequalignorecasedemo/StringEqualIgnoreCaseDemo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringequalignorecasedemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringEqualIgnoreCaseDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s1 = "Sunjid";
+        String s2 = new String ("sunjid");
+        System.out.println("s1 : "+s1);
+        System.out.println("s2 : "+s2);
+        
+        int length = s1.length();
+        System.out.println("Length of S1 : "+length);
+        
+        length = s2.length();
+        System.out.println("Length of S2 : "+length);
+        
+        if(s1.equalsIgnoreCase(s2)) 
+        {
+            System.out.println("Equal");
+        }
+        else
+        {
+            System.out.println("Not Equal");
+        }
+    }
+    
+}
