f71c01e7adfcbfa118a4a6bd7a15fe6e565679ad
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-30 15:08:18 +0600

    Add files via upload

---
+++ StringCharAtDemo/src/stringcharatdemo/StringCharAtDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringcharatdemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringCharAtDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String country = "My Name is Sunjid Hasan";
+        System.out.println(country);
+        
+        char ch = country.charAt(0);
+        System.out.println(ch);
+    }
+    
+}
