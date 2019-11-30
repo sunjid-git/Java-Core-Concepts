9ff7c0995405c65eacb8f1dd3d44526a848c8b55
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-30 15:25:15 +0600

    Add files via upload

---
+++ StringFindingLengthDemo/src/stringfindinglengthdemo/StringFindingLengthDemo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringfindinglengthdemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringFindingLengthDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String s1 = " Sunjid";
+        String s2 = new String("Hasan");
+           
+        System.out.println(" s1 : "+s1);
+        System.out.println(" s2 : "+s2);
+        
+        int length = s1.length();
+        System.out.println("Length of s1 : "+length);
+        
+        length = s2.length();
+        System.out.println("Length of s2 : "+length);
+        
+      
+    }
+    
+}
