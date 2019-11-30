167c05ae19b24e5afa3cdae9ff7ce0ecf880d7e4
Sunjid Hasan <sunjid.info@gmail.com>
2019-11-30 15:24:39 +0600

    Add files via upload

---
+++ StringEqualsMethodDemo/src/stringequalsmethoddemo/StringEqualsMethodDemo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringequalsmethoddemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringEqualsMethodDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String s1 = "Sunjid";
+        String s2 = new String ("Sunjid");
+        System.out.println("s1 : "+s1);
+        System.out.println("s2 : "+s2);
+        
+        int length = s1.length();
+        System.out.println("Length of S1 : "+length);
+        
+        length = s2.length();
+        System.out.println("Length of S2 : "+length);
+        
+        if(s1.equals(s2))
+        {
+            System.out.println("Equal");
+        }
+        else
+        {
+            System.out.println("Not Equal");
+        }
+        
+       
+    }
+    
+}
