bae0b574b65ee2aaa6eb75597c04c6e8b01c1d19
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-01 00:40:35 +0600

    Add files via upload

---
+++ StringIntroductionDemo/src/stringintroductiondemo/StringIntroductionDemo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringintroductiondemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringIntroductionDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String s1 = " Sunjid";
+        String s2 = new String("Hasan");
+           
+        System.out.println(" s1 : "+s1);
+        System.out.println(" s2 : "+s2);
+        
+    }
+    
+}
