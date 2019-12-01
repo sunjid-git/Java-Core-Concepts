639917bfc6d8a6822746e05dd02f6dcc1288ce04
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-02 00:38:12 +0600

    Add files via upload

---
+++ MathClassRemake/src/Math/MathDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Math;
+
+/**
+ *
+ * @author Asus
+ */
+public class MathDemo {
+    public static void main(String[] args) {
+       int x=  Math.abs(-9);
+        System.out.println("X : "+x);
+        
+        
+        
+        
+    }
+}
