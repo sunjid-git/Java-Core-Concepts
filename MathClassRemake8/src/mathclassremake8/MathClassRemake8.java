8d5ef9480d1af34e0f0e58cea89e92be969fb099
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-03 00:40:11 +0600

    Add files via upload

---
+++ MathClassRemake8/src/mathclassremake8/MathClassRemake8.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathclassremake8;
+
+/**
+ *
+ * @author Asus
+ */
+public class MathClassRemake8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Math.max(-3, 3));
+        System.out.println(Math.max(3, 5));
+        System.out.println(Math.max(-2, 1));
+        System.out.println(Math.max(-4, 6));
+        System.out.println(Math.max(8, 3));
+    }
+    
+}
