7104237b2b85ccb59abc3e6d3c9dc2913e6054e3
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-03 00:41:44 +0600

    Add files via upload

---
+++ MathClassRemake11/src/mathclassremake11/MathClassRemake11.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathclassremake11;
+
+/**
+ *
+ * @author Asus
+ */
+public class MathClassRemake11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Math.floor(-8.5));
+        System.out.println(Math.floor(8.6));
+        System.out.println(Math.floor(-8.3));
+        System.out.println(Math.floor(-8.49));
+        System.out.println(Math.floor(-8.9));
+    }
+    
+}
