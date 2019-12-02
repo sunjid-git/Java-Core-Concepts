d678402057c506a4d32dd986d18d6531936cf348
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-03 00:46:57 +0600

    Add files via upload

---
+++ StaticMethodIntroduction/src/StaticMethod/StaticMethod.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StaticMethod;
+
+/**
+ *
+ * @author Asus
+ */
+public class StaticMethod {
+    void display1()
+    {
+        System.out.println("I am static method");
+    }
+    static void display2()
+    {
+        System.out.println("I am not static method");
+    }
+}
