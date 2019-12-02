df9e55fe5b895fe4adb04d67f54aff80d018be3a
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-03 00:48:07 +0600

    Add files via upload

---
+++ StaticVariableExample1/src/StaticExample/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StaticExample;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+        System.out.println("Name : "+Static.UniversityName);
+        
+    }
+}
