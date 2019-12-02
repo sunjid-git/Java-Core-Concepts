84fe52ef1eae3b00c54d390252a81aab8011a4c0
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-03 00:40:44 +0600

    Add files via upload

---
+++ MathClassRemake9/src/mathclassremake9/MathClassRemake9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathclassremake9;
+
+/**
+ *
+ * @author Asus
+ */
+public class MathClassRemake9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Math.min(5, 2));
+        System.out.println(Math.min(7, 0));
+        System.out.println(Math.min(8, 1));
+        System.out.println(Math.min(6, 5));
+        System.out.println(Math.min(2, 3));
+    }
+    
+}
