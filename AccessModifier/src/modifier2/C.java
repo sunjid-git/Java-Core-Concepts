b61ac3d476a92f3514952b11f10e3dc34a8a26b4
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-04 01:06:26 +0600

    Add files via upload

---
+++ AccessModifier/src/modifier2/C.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modifier2;
+import modifier1.A;
+/**
+ *
+ * @author Asus
+ */
+public class C {
+    public static void main(String[] args) {
+        A a = new A();
+        a.display();
+    }
+}
