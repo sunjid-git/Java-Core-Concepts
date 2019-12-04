dd077c2fb5a282b24a2d53bf7bfedaa8b6dddde0
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-05 00:38:14 +0600

    Add files via upload

---
+++ This3/src/This/Person.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package This;
+
+/**
+ *
+ * @author Asus
+ */
+public class Person {
+    String name;
+    int age;
+    String hairColor;
+    
+    Person(String name, int age)
+    {
+        this.name =name;
+        this.age =age;
+    }
+    
+    Person(String name, int age, String hairColor)
+    {
+        this(name,age);
+        this.hairColor = hairColor;
+    
+    }
+    void display()
+    {
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("Haircolor : "+hairColor);
+    }
+}
---
+++ This3/src/This/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package This;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person("Sunjid", 25);
+        p1.display();
+        
+        Person p2 = new Person("Hasan", 30,"Black");
+        p2.display();
+    }
+}
