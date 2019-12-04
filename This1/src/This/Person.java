c3cb3d51f8b3e607de5b464303d81a5a9b4fad46
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-05 00:39:16 +0600

    Add files via upload

---
+++ This1/src/This/Person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package This;
+
+/**
+ *
+ * @author Asus
+ */
+public class Person {
+    String name;
+    int age;
+    
+    
+    Person(String name, int age)
+            //Person(String n, int a)
+    {
+        //name =n;
+        //age =a;
+        this.name =name;
+        this.age =age;
+        
+    }
+    void display()
+    {
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+    }
+}
