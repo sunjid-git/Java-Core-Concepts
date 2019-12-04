bf2f621cd36869bdfc70d5ebfc2b9fe33f4652bd
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-05 00:39:48 +0600

    Add files via upload

---
+++ This/src/This/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package This;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person("Sunjid", 25);
+        p1.display();
+    }
+}
