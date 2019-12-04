40fef02a7cdffb2f22935def9d4e2a8d1ab4cb86
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-05 00:37:40 +0600

    Add files via upload

---
+++ This4/src/This/Person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package This;
+
+/**
+ *
+ * @author Asus
+ */
+public class Person {
+    void message()
+    {
+        System.out.println("I am Message method");
+    }
+    
+    
+    void display()
+    {
+        this.message();
+        System.out.println("I am Display method");
+    }
+}
