dd32d9c9c1d57b555fef496334ace097a3ec24c9
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-05 00:34:34 +0600

    Add files via upload

---
+++ TypeUpCasting/src/Up/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Up;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Teacher(); // up casting
+        p.display(); 
+    }
+}
