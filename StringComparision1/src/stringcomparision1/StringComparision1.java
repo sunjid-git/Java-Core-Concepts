2951ea68f63080378a1d24bb36feff4556ef5310
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-06 21:25:45 +0600

    Add files via upload

---
+++ StringComparision1/src/stringcomparision1/StringComparision1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringcomparision1;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringComparision1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String password1 = "Sunjid123456";
+        String password2 = "Sunjid123456";
+        String password3 = new String("Sunjid123456");
+        String password4 = new String("Sunjid123456");
+        
+        System.out.println(password1.equals(password2));
+        System.out.println(password1.equals(password3));
+        System.out.println(password1.equals(password4));
+    }
+    
+}
