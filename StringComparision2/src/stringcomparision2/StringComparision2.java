cac21aa2ecac994bb7503a77215806c187ec6b95
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-06 21:26:09 +0600

    Add files via upload

---
+++ StringComparision2/src/stringcomparision2/StringComparision2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringcomparision2;
+
+/**
+ *
+ * @author Asus
+ */
+public class StringComparision2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String password1 = "Sunjid123456";
+        String password2 = "Sunjid123456";
+        String password3 = new String("Sunjid123456");
+        String password4 = new String("Sunjid12345");
+        
+        System.out.println(password1.equals(password2));
+        System.out.println(password1.equals(password3));
+        System.out.println(password1.equals(password4));
+    }
+    
+}
