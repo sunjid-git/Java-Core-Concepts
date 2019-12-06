7be5db7652594c241303841692c8437ae2b563e5
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-06 21:29:25 +0600

    Add files via upload

---
+++ SuperClass3/src/Super/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Super;
+
+/**
+ *
+ * @author Asus
+ */
+public class B {
+    B()
+    {
+        System.out.println("B's a Constructor");
+    }
+}
