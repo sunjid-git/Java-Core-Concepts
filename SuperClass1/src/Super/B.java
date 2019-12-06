5a154a031c1475c5d9f829641b9998c7281ac6a1
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-06 21:28:23 +0600

    Add files via upload

---
+++ SuperClass1/src/Super/B.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Super;
+
+/**
+ *
+ * @author Asus
+ */
+public class B extends A{ 
+    //display()
+    @Override
+    void display()
+    {
+        super.display();
+        System.out.println("Inside B Class");
+    }
+}
