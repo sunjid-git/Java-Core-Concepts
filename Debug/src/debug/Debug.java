be0bea7f7d0dd72fa3a8dc1f1756e8926ea5974d
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-09 00:29:01 +0600

    Add files via upload

---
+++ Debug/src/debug/Debug.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debug;
+
+/**
+ *
+ * @author Asus
+ */
+public class Debug {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int sum=0;
+        for (int i = 0; i <= 10; i++) {
+            sum=sum+i;
+        }
+        System.out.println("SUM : "+sum);
+    }
+    }
+    
+
