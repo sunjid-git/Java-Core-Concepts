bc88e8b4baa990da01148bef7cefc5367915d48d
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-09 00:29:35 +0600

    Add files via upload

---
+++ DecimalNumberFormat/src/decimalnumberformat/DecimalNumberFormat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decimalnumberformat;
+
+/**
+ *
+ * @author Asus
+ */
+public class DecimalNumberFormat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double x = 2.987654;
+        System.out.printf("X : %.2f",x);
+        System.out.println();
+    }
+    
+}
