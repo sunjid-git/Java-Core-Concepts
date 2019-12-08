16ee092071855ce4ba2c91f4788bb06cebbf2094
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-09 00:27:50 +0600

    Add files via upload

---
+++ Anonymous/src/Anonymous/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Anonymous;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person()
+        {
+            @Override
+            void display()
+            {
+                System.out.println("Test Class");
+            }
+        
+        };
+        p.display();
+    }
+}
