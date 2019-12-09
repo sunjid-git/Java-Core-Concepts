aa0486dea8bb8a22bbbad780af8d90181a312a74
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-10 01:10:53 +0600

    Add files via upload

---
+++ Encapsulation/src/Encapsulation/Person.java
@@ -0,0 +1,13 @@
+
+package Encapsulation;
+
+public class Person {
+    String name;
+    int age;
+    
+    void display()
+    {
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+    }
+}
---
+++ Encapsulation/src/Encapsulation/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encapsulation;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person ob = new Person();
+        ob.name = "Sunjid";
+        ob.age = 25;
+        ob.display();
+    }
+}
