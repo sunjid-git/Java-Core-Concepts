2e8d8c9be2d50f3ee86616919c64e2a7e625778e
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-11 02:10:07 +0600

    Add files via upload

---
+++ ExceptionClassNotFound/src/exceptionclassnotfound/ExceptionClassNotFound.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionclassnotfound;
+
+/**
+ *
+ * @author Asus
+ */
+public class ExceptionClassNotFound {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Class class = new Class();
+        System.out.println(class);
+        
+    }
+    
+}
