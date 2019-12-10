ea9793aad74697a02f68e2bde8dffe4a40098833
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-11 02:10:36 +0600

    Add files via upload

---
+++ ExceptionDemo/src/exceptiondemo/ExceptionDemo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptiondemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class ExceptionDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;
+        int y = 0;
+        int result =x/y;
+        System.out.println("Result : "+result);
+        System.out.println("Last line of the program");
+        
+    }
+    
+}
