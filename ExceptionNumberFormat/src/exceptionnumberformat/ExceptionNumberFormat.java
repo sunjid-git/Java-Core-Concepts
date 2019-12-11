3b75d046006ae08af57a1829a3d49a6152fac8cd
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-12 01:52:46 +0600

    Add files via upload

---
+++ ExceptionNumberFormat/src/exceptionnumberformat/ExceptionNumberFormat.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionnumberformat;
+
+/**
+ *
+ * @author Asus
+ */
+public class ExceptionNumberFormat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num = Integer.parseInt("Sunjid");
+        System.out.println(num);
+    }
+    
+}
