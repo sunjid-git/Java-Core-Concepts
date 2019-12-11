64e529ea6a5bf9ef5f2402bea85451d5e2915744
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-12 01:52:03 +0600

    Add files via upload

---
+++ ExceptionNull/src/exceptionnull/ExceptionNull.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionnull;
+
+/**
+ *
+ * @author Asus
+ */
+public class ExceptionNull {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = null;
+        System.out.println(name.charAt(0));
+    }
+    
+}
