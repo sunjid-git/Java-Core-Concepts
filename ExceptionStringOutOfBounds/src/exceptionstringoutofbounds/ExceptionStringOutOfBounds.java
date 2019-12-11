dd1d763f5e2a701df510a34b6395457a9563a644
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-12 01:53:27 +0600

    Add files via upload

---
+++ ExceptionStringOutOfBounds/src/exceptionstringoutofbounds/ExceptionStringOutOfBounds.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionstringoutofbounds;
+
+/**
+ *
+ * @author Asus
+ */
+public class ExceptionStringOutOfBounds {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Sunjid";
+        System.out.println(name.charAt(7));
+    }
+    
+}
