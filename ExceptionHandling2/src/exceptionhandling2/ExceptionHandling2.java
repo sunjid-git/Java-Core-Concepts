f028a792bc35c4be517352288ceb4196a64cf599
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-12 00:19:36 +0600

    Add files via upload

---
+++ ExceptionHandling2/src/exceptionhandling2/ExceptionHandling2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionhandling2;
+
+/**
+ *
+ * @author Asus
+ */
+public class ExceptionHandling2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try{
+         int x = 10;
+        int y = 0;
+        int result =x/y;
+        System.out.println("Result : "+result);
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Exception : "+e);
+        }
+        catch(ArithmeticException f)
+        {
+            System.out.println("Exception : "+f);
+        }
+        finally
+        {
+        System.out.println("Last line of the program");
+        }
+    }
+    
+}
