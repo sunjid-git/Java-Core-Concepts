f99d6e39b6fac1eb6f301f08122a4d5e547050e2
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-13 00:22:44 +0600

    Add files via upload

---
+++ FileDirectoryCreate/src/filedirectorycreate/FileDirectoryCreate.java
@@ -0,0 +1,21 @@
+
+package filedirectorycreate;
+
+import java.io.File;
+
+
+public class FileDirectoryCreate {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //File directory= new File("C:\\Users\\Asus\\Desktop\\Sunjid");
+       // File directory= new File("C:/Users/AsusDesktop/Sunjid");
+        File directory= new File("Sunjid");
+        directory.mkdir(); // to create directory
+        
+        
+        
+    }
+    
+}
