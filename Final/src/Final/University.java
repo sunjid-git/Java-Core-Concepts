4c516a05c1afb92f423d9fa48a0749e499f99975
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-19 01:47:29 +0600

    Add files via upload

---
+++ Final/src/Final/University.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Final;
+
+/**
+ *
+ * @author Asus
+ */
+public class University {
+    
+    final String name = "BAIUST";
+    final String PLACE = "Cumilla";
+    int fee = 85000;
+    void display()
+    {
+       
+        System.out.println("Name : "+name);
+        System.out.println("Fee : "+fee);
+        System.out.println("Place : "+PLACE);
+    }
+}
