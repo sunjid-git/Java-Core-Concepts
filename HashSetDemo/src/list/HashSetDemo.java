0ec701c780195dcc64c3405662b8cce5055f0958
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-19 01:54:31 +0600

    Add files via upload

---
+++ HashSetDemo/src/list/HashSetDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package list;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Asus
+ */
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<String> fruitName = new HashSet<String>();
+        fruitName.add("Sunjid");
+        fruitName.add("Hasan");
+        fruitName.add("Mafi");
+        
+        System.out.println(fruitName);
+    }
+}
