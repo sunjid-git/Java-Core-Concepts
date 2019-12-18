226d615596c80fb7192151fee39d78e1c458e520
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-19 01:50:44 +0600

    Add files via upload

---
+++ Final3/src/Final/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Final;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+       Student ob = new Student();
+        ob.display();
+      
+        ob.display1();
+    }
+}
