323ae18441a15320468c831788bb82ef3a100732
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-20 02:35:34 +0600

    Add files via upload

---
+++ Interface1/src/Interface/C.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Asus
+ */
+public class C implements A,B {
+    public void play()
+    {
+        System.out.println("Hell, I am from C");
+    }
+    
+}
