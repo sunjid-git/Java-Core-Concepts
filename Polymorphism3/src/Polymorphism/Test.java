47ee235c3428dc4757ee52d911ff453dd3ac4823
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-23 09:12:28 +0600

    Add files via upload

---
+++ Polymorphism3/src/Polymorphism/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+    public static void main(String[] args) {
+        Shape ob = new Shape();
+        Shape ob1 = new Rectangle(10,20);
+        Shape ob2 = new Triangle(10,20);
+        
+        System.out.println("Shape : "+ob.area());
+        System.out.println("Shape Rectangle : "+ob1.area());
+        System.out.println("Shape Triangle : "+ob2.area());
+        
+        
+    }
+}
