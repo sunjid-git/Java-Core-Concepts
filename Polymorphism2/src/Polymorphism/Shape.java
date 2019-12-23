d5b62d005df674e4b4d623c0af7d7e058193f68e
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-23 09:11:50 +0600

    Add files via upload

---
+++ Polymorphism2/src/Polymorphism/Shape.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Asus
+ */
+public class Shape {
+    double area()
+    {
+    return 0;
+    }
+}
