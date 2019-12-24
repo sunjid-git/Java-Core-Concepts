e826ea711898da11f8c806b7349f9d1c4945b70e
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-24 20:23:33 +0600

    Add files via upload

---
+++ OopConstructorDefault1/src/Teacher.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class Teacher {
+    String name, gender;
+                int phone;
+
+            // no default constructor yet it will automatically generate default constructor
+            void displayInformation()
+            {
+            System.out.println("Name : "+name);
+            System.out.println("Gender : "+gender);
+            System.out.println("Phone : "+phone);
+            System.out.println();
+
+            }
+}
---
+++ OopConstructorDefault1/src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+     public static void main(String[] args) {
+        
+        Teacher teacher4= new Teacher();
+        teacher4.displayInformation();
+    }
+}
