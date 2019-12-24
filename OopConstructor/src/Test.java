2099d6743af3d44a6cec7a54e6b1166a59f81ea4
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-24 20:22:21 +0600

    Add files via upload

---
+++ OopConstructor/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher("Sunjid","Male",01521);
+        teacher1.displayInformation();
+        
+        Teacher teacher2 = new Teacher("Hasan","Male",01770);
+        teacher2.displayInformation();
+        
+        Teacher teacher3 = new Teacher("Mafi","Male",01732);
+        teacher3.displayInformation();
+        
+    }
+}
