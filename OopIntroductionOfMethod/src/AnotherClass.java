d2b1fe89890ef36973d40e899027596d3f7b68d6
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-24 20:24:43 +0600

    Add files via upload

---
+++ OopIntroductionOfMethod/src/AnotherClass.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class AnotherClass {
+    public static void main(String[] args) {
+       
+        //teacher1
+        Teacher teacher1;//object declacre
+        teacher1= new Teacher(); //object created
+        teacher1.name="Sunjid";
+        teacher1.gender="Male";
+        teacher1.phone=1521417063;
+        teacher1.displayInformation();
+        
+        //teacher2
+        Teacher teacher2;//object declacre
+        teacher2= new Teacher(); //object created
+        teacher2.name="Hasan";
+        teacher2.gender="Male";
+        teacher2.phone=1770981999;
+        teacher2.displayInformation();
+
+        //teacher2
+        Teacher teacher3;//object declacre
+        teacher3= new Teacher(); //object created
+        teacher3.name="Hasan";
+        teacher3.gender="Male";
+        teacher3.phone=1770981999;
+        teacher3.displayInformation();
+    }
+}
---
+++ OopIntroductionOfMethod/src/Teacher.java
@@ -0,0 +1,13 @@
+
+public class Teacher {
+    String name ,gender;
+        int phone;
+        
+     void displayInformation()
+     {
+        System.out.println("Name : "+name);
+        System.out.println("Name : "+gender);
+        System.out.println("Name : "+phone);
+         System.out.println();
+     }
+}
