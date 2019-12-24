847e0b66a8b7bfd76afe80f6bb0303b1d2100273
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-25 02:02:50 +0600

    Add files via upload

---
+++ OopStaticBlock/src/StaticBlock/StaticTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StaticBlock;
+
+/**
+ *
+ * @author Asus
+ */
+public class StaticTest {
+    public static void main(String[] args) {
+        //to call no object has to call , in main static is used
+        StaticBlock.display();
+    }
+}
