8df9938fa2771811aaa34c7b3b55eb05e8de9af1
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-25 02:04:17 +0600

    Add files via upload

---
+++ OopStaticMethod/src/StaticMethod/StaticMethod.java
@@ -0,0 +1,13 @@
+
+package StaticMethod;
+
+public class StaticMethod {
+    void display1()
+    {
+        System.out.println("I am non Static Method");
+    }
+    static void display2()
+    {
+        System.out.println("I am Static Method");
+    }
+}
---
+++ OopStaticMethod/src/StaticMethod/Test.java
@@ -0,0 +1,12 @@
+
+package StaticMethod;
+
+
+public class Test {
+    public static void main(String[] args) {
+         StaticMethod ob1 = new StaticMethod();
+         ob1.display1();
+         
+         StaticMethod.display2(); // class name.method
+    }
+}
