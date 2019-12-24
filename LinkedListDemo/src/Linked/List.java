dd27c20f20566bf2a247c36bd4e3b4fe90d2330d
Sunjid Hasan <sunjid.info@gmail.com>
2019-12-24 20:19:46 +0600

    Add files via upload

---
+++ LinkedListDemo/src/Linked/List.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Linked;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Asus
+ */
+public class List {
+    public static void main(String[] args) {
+        LinkedList<String>  countryName = new LinkedList<String>();
+        countryName.add("Sunjid");
+        countryName.add("Hasan");
+        countryName.add("Mafi");
+        System.out.println(countryName);
+    }
+}
